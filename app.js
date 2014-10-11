b0bd22998fab134277ef06edf4091ffc653adc41
Puru Dahal <pdahal@me.com>
2014-10-11 13:30:53 -0700

    Add database and routes module and require them accordingly

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // Require Dependencies
 var express           = require('express'),
     mongoose          = require('mongoose'),
-    config            = require('./config')
+    config            = require('./config'),
+    app               = express()
+
+// Setup port
+var port = '3000'
+
+// Start the Server
+app.listen(port)
+console.log('TwitterArticle loading on: http://localhost:' + port)
